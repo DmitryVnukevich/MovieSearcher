dad1ffaec4239f0a64707ff6716b87c780650f1b
Dmitry Vnukevich <vnukevich_da_21@mf.grsu.by>
2024-06-04 18:55:37 +0300

    Initial commit

---
+++ src/test/java/com/example/moviesearcher/MoviesearcherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.moviesearcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesearcherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
