584296ebf49452aee323cafd42a9eb84fcded917
Dmitry Vnukevich <vnukevich_da_21@mf.grsu.by>
2024-06-12 22:15:44 +0300

    duplicated files have been deleted

--- src/main/resources/db/changelog/create/sql/create-genre-table.sql
+++
@@ -1,4 +0,0 @@
-CREATE TABLE genre (
-                        id BIGSERIAL PRIMARY KEY,
-                        name VARCHAR(50) NOT NULL UNIQUE
-);
--- src/main/resources/db/changelog/create/sql/create-movie-table.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE movie (
-                        id BIGSERIAL PRIMARY KEY,
-                        title VARCHAR(100) NOT NULL,
-                        description TEXT,
-                        release_date DATE NOT NULL,
-                        duration INTERVAL NOT NULL,
-                        poster_url TEXT
-);
